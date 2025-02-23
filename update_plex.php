089e22b4198c43b603231e707f8ae59eacaf722d
Jack <jwiemmolo@gmail.com>
2025-02-23 18:24:51 +0800

    Formatting

--- update_plex.php
+++ update_plex.php
@@ -6,22 +6,16 @@ use GuzzleHttp\Client;
 $plexToken = getenv('PLEX_TOKEN');
 $plexUrl = getenv('PLEX_URL');
 
-echo "Debug: Using URL: {$plexUrl}\n";
-echo "Debug: Token present: " . (!empty($plexToken) ? 'Yes' : 'No') . "\n";
-
 try {
     $client = new GuzzleHttp\Client([
         'headers' => [
             'Accept' => 'application/json',
             'X-Plex-Token' => $plexToken
         ],
-        'timeout' => 5 // Lower timeout to fail faster
+        'timeout' => 5
     ]);
-    
-    echo "Debug: Making request to: {$plexUrl}/library/recentlyAdded\n";
-    
+        
     $response = $client->request('GET', $plexUrl . '/library/recentlyAdded');
-    
     
     $data = json_decode($response->getBody(), true);
     
